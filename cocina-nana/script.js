e1bb19ad639b9b2e600fc8e67ae8928f5f8edae6
Juan Camilo Del Busto <98706692+camijam@users.noreply.github.com>
2022-02-08 10:16:23 +0100

    first add

---
+++ cocina-nana/script.js
@@ -0,0 +1,8 @@
+/*menu reactivo*/
+
+document.querySelector('.menu').addEventListener
+('click',() => {
+    document.querySelectorAll('.target').forEach((item) =>{
+        item.classList.toggle('change')
+    })
+})
